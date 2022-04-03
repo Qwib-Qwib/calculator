06b5127cc7b145374b7bebf8518b08c64b564a6b
Qwib-Qwib <jp93rom@orange.fr>
2022-04-03 20:30:32 +0200

    Code the entirety of the CSS and HTML, and most of the Javascript. Only the erase buttons remain, as well as the decimal and the color related ones, the keyboard support and the overflowing prevention feature.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # calculator
+
+Lorsqu'on appuie sur une touche d'opérateur...
+    Si num1 est vide ou égale à 0, et si la touche est un + ou un -...
+        on passe num2 à num1.
+        on met à jour l'opérateur.
+        on vide num2.
+        on vide l'arrayNum2.
+        on met à jour l'affichage.
+    Si num1 est vide et si la touche est un X...
+            on appelle multiply(1, num2).
+            on met à jour l'opérateur.
+            on met à jour l'affichage.
+    Si num1 n'est pas vide...
+        on vérifie quel est l'opérateur actuel...
+            Si c'est le même opérateur...
+                rien ne se passe.
+            Si l'opérateur est différent...
+
+Lorsqu'on appuie sur la touche EXE...
+    on lit l'opérateur.
+    ON appele la fonction correspondante.
+    On met à jour l'affichage.
+
+Si EXE a été touché juste avant d'appuyer sur un opérateur, num1 doit recevoir le résultat de l'opération précédente.
+
+Si un nouvel opérateur est entré alors que num2 est vide, l'opérateur doit juste être mis à jour.
+
+Si num1 est négatif, il devrait être entouré de parenthèses dans l'affichage userInput, mais attention à ce que ça n'interfère pas avec les calculs.
+
+IL va sans doute falloir réécrire operate et les opérations pour que la conversion en chiffres se fasse seulement dans operate et qu'elle soit utilisable à chaque fois qu'il faille réaliser une quelconque opération.
+
+
+Quand _____ ?
+    Dans le cas d'une addition ou d'une soustraction, opérateur mis à jour.
+    Dans le cas d'une multiplication ou d'une division, num1 est 1 par défaut, opérateur mis à jour.
+
+Quand num1 et l'opérateur ont une valeur, mais pas num2 ?
+    num1 et num2 restent tels quels, opérateur mis à jour.
+
+Quand num1, l'opérateur et num2 ont une valeur ?
+    L'opérateur déjà présent est lu, l'opération appropriée est réalisée, puis le résultat est envoyé dans num1 ainsi que dans le VRAI résultat, puis num2 et son array sont vidés et l'opérateur mis à jour.
+
+Quand EXE vient d'être pressé ?
+    Le résultat original est envoyé en num1, num2 et son array sont vidés, et l'opérateur est mis à jour.
+
+    RESTE A CORRIGER L'AFFICHAGE DES NOMBRES NEGATIFS!
--- script.js
+++ script.js
@@ -3,7 +3,8 @@ buttons.forEach(groupAddEventListener);
 let num1 = "";
 let num2 = "";
 let operatorSign = "";
-let arrayNum2 =[]; 
+let arrayNum2 = []; 
+let arrayDisplay = [num1, operatorSign, num2, arrayNum2];
 
 function add(x, y) {
     let addResult = x + y;
@@ -23,19 +24,21 @@ function multiply(x, y) {
 function divide(x, y) {
     let divideResult = x / y;
     if (y == 0) {
-        return "SNARKY ERROR MESSAGE!";
+        return "I AM ERROR";
     } else {
         return divideResult;
     }
 }
 
 function operate(x, y, z) {
+    x = Number(x);
+    y = Number(y);
     let result;
     if (z === "+") {
         result = add(x, y);
     } else if (z === "-") {
         result = substract(x, y);
-    } else if (z === "*") {
+    } else if (z === "X") {
         result = multiply(x, y);
     } else if (z === "/") {
         result = divide(x, y);
@@ -50,96 +53,130 @@ function groupAddEventListener(element) {
 function displayOperation(event) {
     const buttonPressed = event.currentTarget;
     let userInput = document.getElementById("user-input");
+    let currentResult = document.getElementById("result");
     const buttonClasses = buttonPressed.classList;
     let displayedInput;
     if (buttonClasses[1] === "one") {
         arrayNum2.push("1");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "two") {
         arrayNum2.push("2");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     }  else if (buttonClasses[1] === "three") {
         arrayNum2.push("3");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "four") {
         arrayNum2.push("4");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "five") {
         arrayNum2.push("5");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "six") {
         arrayNum2.push("6");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "seven") {
         arrayNum2.push("7");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "eight") {
         arrayNum2.push("8");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "nine") {
         arrayNum2.push("9");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "zero") {
         arrayNum2.push("0");
         num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "plus") {
-        arrayNum2.push("2");
-        num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        if (num1 === "" || num1 === "0") {
+            num1 = num2;
+        } else if (operatorSign !== "" && num1 !== "" && num2 !== "") {
+            num1 = operate(num1, num2, operatorSign);
+            currentResult.textContent = num1;
+        }
+        operatorSign = "+";
+        num2 = "";
+        arrayNum2 = [];
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "minus") {
-        arrayNum2.push("2");
-        num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        if (num1 === "" || num1 === "0") {
+            num1 = num2;
+        } else if (operatorSign !== "" && num1 !== "" && num2 !== "") {
+            num1 = operate(num1, num2, operatorSign);
+            currentResult.textContent = num1;
+        }
+        operatorSign = "-";
+        num2 = "";
+        arrayNum2 = [];
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "multiply") {
-        arrayNum2.push("2");
-        num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        if (num2 === "" && num1 === "") {
+            num1 = 1;
+        } else if (num2 !== "" && num1 == "" && operatorSign == "") {
+            num1 = num2;
+        } else if (num2 !== "" && num1 !== "" && operatorSign !== "") {
+            num1 = operate(num1, num2, operatorSign);
+            currentResult.textContent = num1;
+        }
+        operatorSign = "X";
+        num2 = "";
+        arrayNum2 = [];
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
     } else if (buttonClasses[1] === "divide") {
-        arrayNum2.push("2");
-        num2 = arrayNum2.reduce(aggregateInput);
-        displayedInput = num1 + operatorSign + num2;
-        userInput.textContent = displayedInput;
-        return arrayNum2;
+        if (num2 === "" && num1 === "") {
+            num1 = 1;
+        } else if (num2 !== "" && num1 == "" && operatorSign == "") {
+            num1 = num2;
+        } else if (num2 !== "" && num1 !== "" && operatorSign !== "") {
+            num1 = operate(num1, num2, operatorSign);
+            currentResult.textContent = num1;
+        }
+        operatorSign = "/";
+        num2 = "";
+        arrayNum2 = [];
+        updateUserInput(num1, num2, operatorSign, userInput);
+        return arrayDisplay;
+    } else if (buttonClasses[1] === "exe") {
+        let result = operate(num1, num2, operatorSign);
+        updateUserInput(num1, num2, operatorSign, userInput);
+        currentResult.textContent = result;
+        arrayNum2 = [];
+        return arrayDisplay;
     }
 }
 
 
 function aggregateInput(previousInput, currentInput) {
     return currentInput = previousInput + currentInput;
+}
+
+function updateUserInput(num1, num2, operatorSign, userInput) {
+    if (num1 < 0) {
+        displayedInput = `(${num1})` + operatorSign + num2;
+    } else {
+        displayedInput = num1 + operatorSign + num2;
+    }
+    userInput.textContent = displayedInput;
 }
